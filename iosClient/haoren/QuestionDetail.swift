3b86e086fe02b7fa0f030118ed94465edc641119
zd <mengzide@gmail.com>
2015-05-13 00:07:45 +0200

    question detail

---
+++ iosClient/haoren/QuestionDetail.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionDetail.swift
+//  haoren
+//
+//  Copyright (c) 2015 cooldown. All rights reserved.
+//
+
+import Foundation
+
+class QuestionDetail {
+    
+    var isImage :Bool?
+    var isText: Bool?
+    var isVoice: Bool?
+    
+    
+}
--- iosClient/haoren/QuestionDetailViewController.swift
+++ iosClient/haoren/QuestionDetailViewController.swift
@@ -6,15 +6,20 @@
 //
 
 import Foundation
-import Foundation
 import UIKit
-class QuestionDetailViewController: UIViewController {
+class QuestionDetailViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var questionTitle: UILabel!
     
+    @IBOutlet weak var questionUserImageView: UIImageView!
+    @IBOutlet weak var questionDetailTableView: UITableView!
     var currentQuestion : Question?
+    var questionDetailTable: [QuestionDetail] = [QuestionDetail] ()
     
+    @IBOutlet weak var buttonReply: UIButton!
 
+    @IBOutlet weak var buttonUpVote: UIButton!
+    @IBOutlet weak var buttonDownVote: UIButton!
     @IBOutlet weak var questionLocation: UILabel!
     @IBOutlet weak var questionCreateTime: UILabel!
     @IBOutlet weak var questionUser: UILabel!
@@ -24,9 +29,15 @@ class QuestionDetailViewController: UIViewController {
     
     func initQuestion(){
         self.questionTitle.text = currentQuestion?.questionTitle
+        self.questionTitle.numberOfLines=0
+        self.questionTitle.lineBreakMode = .ByWordWrapping
         self.questionLocation.text = currentQuestion?.questionLocation
         self.questionCreateTime.text = currentQuestion?.questionCreateTime
         self.questionUser.text = currentQuestion?.questionUser
+        self.questionUserImageView.image = UIImage(named: "meinv")
+        
+        
+        
         
         //let backButton = UIBarButtonItem(title: "< Back", style: UIBarButtonItemStyle.Plain, target: self, action: "goBack")
         //self.navigationItem.leftBarButtonItem = backButton
@@ -40,11 +51,46 @@ class QuestionDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.initAppear()
         self.initQuestion()
         
         
         
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.questionDetailTable.count
+    
+    
+    }
+    
+    
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+        
+    }
+    
+    
+    
+    
+    func initAppear() {
+
+        //these lines are used to remove empty seperator liens.
+        self.questionDetailTableView.tableFooterView = UIView(frame: CGRectZero)
+        self.questionDetailTableView.backgroundColor = UIColor.clearColor()
+        //println("wtf+ \(self.questionDetailTableView.frame.size.width)")
+        
+        
+    }
+    override func viewDidLayoutSubviews() {
+        println("QuestionDetailView load subviews")
+        self.navigationController?.navigationBar.barTintColor = UIColor(red: 76.0/255.0, green: 152/255.0, blue: 198/255.0, alpha: 1)
+        
+        //adjust tablevie
+        self.questionDetailTableView.frame = CGRect(x: 0, y: 200, width: self.view.frame.size.width, height: self.view.frame.size.height)
     }
     //确定是这样么？？貌似这样好使。。直接在转场的时候设定
     /*override func viewWillDisappear(animated: Bool) {
